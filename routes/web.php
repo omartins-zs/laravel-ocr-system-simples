d58df0dab9442fbf7800a7d6129f2c8e0c3c4fd4
omartins-zs <gabrielmatheus209@gmail.com>
2026-03-24 19:39:27 -0300

    ✨ feat: criando rotas web do OCR

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\Web\HistoryController;
+use App\Http\Controllers\Web\OcrController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', '/ocr');
+
+Route::get('/ocr', [OcrController::class, 'index'])->name('ocr.index');
+Route::post('/ocr', [OcrController::class, 'store'])->name('ocr.store');
+
+Route::get('/history', [HistoryController::class, 'index'])->name('history.index');
+Route::get('/history/{ocrDocument}', [HistoryController::class, 'show'])->name('history.show');
